97b04a8bd26e2ccef9cc2aa4477d64de386dcf1a
arlohewitt <75036102+arlohewitt@users.noreply.github.com>
2024-03-10 03:59:50 +0000

    Update MainCamera.h

--- RustDMA/SDK/MainCamera.h
+++ RustDMA/SDK/MainCamera.h
@@ -7,11 +7,11 @@ class MainCamera
 	  "Name": "MainCamera_TypeInfo",
 	  "Signature": "MainCamera_c*"
 	*/
-	uint64_t Class = 0x34FC788;
+	uint64_t Class = 0x39BCF00;
 	//Dump.cs / DummyDLL
 	uint64_t StaticField = 0xB8;// Static Padding To Access Static Fields
 	uint64_t Camera = 0x0;// public static Camera mainCamera
-	uint64_t CameraGamoObject = 0x10;
+	uint64_t CameraGameObject = 0x10;
 	uint64_t ViewMatrixOffset = 0x2E4;
 public:
 	MainCamera();
