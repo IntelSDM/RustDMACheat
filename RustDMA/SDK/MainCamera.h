318b3d1ca0a61a8114906713bc9581d71946b244
arlohewitt <75036102+arlohewitt@users.noreply.github.com>
2024-04-07 03:18:47 +0100

    Update MainCamera.h

--- RustDMA/SDK/MainCamera.h
+++ RustDMA/SDK/MainCamera.h
@@ -13,8 +13,8 @@ class MainCamera
 	uint64_t Camera = 0x0;// public static Camera mainCamera
 
 	//As far as i can tell, these haven't changed in years
-	uint64_t CameraGameObject = 0x10; //You can find these in il2cpp dumps, instead use IDA or CE
-	uint64_t ViewMatrixOffset = 0x2E4; //You can find these in il2cpp dumps, instead use IDA or CE
+	uint64_t CameraGameObject = 0x10; //You can't find these in il2cpp dumps, instead use IDA or CE
+	uint64_t ViewMatrixOffset = 0x2E4; //You can't find these in il2cpp dumps, instead use IDA or CE
 public:
 	MainCamera();
 	ViewMatrix GetViewMatrix();
